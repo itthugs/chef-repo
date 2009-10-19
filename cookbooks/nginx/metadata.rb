3252647e8dc45bbe7161861847805a9b21d79ef4
Dan Williams <dan.williams@itthugs.com>
2009-10-19 20:50:26 -0300

    Beginnings of NGINX+Passenger+SSL recepie

---
+++ cookbooks/nginx/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "IT Thugs"
+maintainer_email "support@itthugs.com"
+license          "Apache 2.0"
+description      "Installs/Configures nginx"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
---
+++ cookbooks/nginx/recipes/passenger.rb
@@ -0,0 +1,39 @@
+#
+# Cookbook Name:: nginx
+# Recipe:: default
+#
+# Copyright 2009, IT Thugs
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+unless File.exists?('/opt/nginx/conf/nginx.conf') do
+  script "install_nginx" do
+    interpreter "bash"
+    user "root"
+    cwd "/tmp"
+    code <<-EOH 
+    wget http://sysoev.ru/nginx/nginx-0.7.62.tar.gz
+    tar -zxf nginx-0.7.62.tar.gz
+    passenger-install-nginx-module --auto --nginx-source-dir=/tmp/nginx-0.7.62 --extra-configure-flags=--with-http_ssl_module --prefix=#{node[:nginx][:dir]}
+    EOH
+  end
+end
+
+template "nginx.conf" do
+  path "#{node[:nginx][:dir]}/nginx.conf"
+  source "nginx.conf.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
